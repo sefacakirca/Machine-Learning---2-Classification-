11a88ff4826c58569cfcd7d90ece9e5fb8a9b3a4
Sefa Cakirca <cakircasefa34@gmail.com>
2026-08-16 17:25:42 +0300

    Validate değerleri oluşturuldu

--- machine_learning_2.py
+++ machine_learning_2.py
@@ -4,5 +4,16 @@
 
 
 """
+import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
 
-print("deneme")
+X,y = load_iris(return_X_y=True)
+
+X_train_val, X_test ,y_train_val, y_test=train_test_split(X,y,test_size=0.2, random_state=42, stratify=y)
+
+X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=0.25,random_state=42,stratify=y_train_val)
+
